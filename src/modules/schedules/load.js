0006ecd0eaf95bc43abec3bd8036873204e5f765
Douglas <douglas.henriqueamaral@gmail.com>
2025-02-28 14:52:26 -0300

    Renderizando os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,7 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import {hoursLoad} from "../form/hours-load.js"
+import { schedulesShow } from "../schedules/show.js"
+
 
 //Seleciona o input da data.
 const selectedDate = document.getElementById("date")
@@ -10,7 +12,10 @@ export async function schedulesDay(){
 
   // Busca na API os agendamentos
   const dailySchedules = await scheduleFetchByDay({ date })
-  console.log(dailySchedules)
+  
+  // Exibe os agendamentos
+  schedulesShow({dailySchedules})
+
   // Renderiza as horas disponíveis.
   hoursLoad({ date })
 
